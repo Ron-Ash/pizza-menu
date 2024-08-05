f660bba298f396f21e4c280bf5038edd74bbf75f
Ron-Ash <ashri.ron@gmail.com>
2024-08-05 13:59:17 +1000

    added starter data and made first simple component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Pizza />
+    </>
+  );
+}
+
+function Pizza() {
+  return (
+    <>
+      <h2>Pizza Spinaci</h2>
+      <img src="pizzas/spinaci.jpg" alt="Spinaci Pizza img" />
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </>
+  );
 }
 
 // React >= v18
